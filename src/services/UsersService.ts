b119c0d29502850be122d71a792d730590f31bc5
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-04-21 18:38:31 -0300

    finish day 3

---
+++ src/services/UsersService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UsersService {
+  private usersRepository: Repository<User>;
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+  async create(email: string) {
+    // Verificar se o usuario existe
+
+    const userExists = await this.usersRepository.findOne({
+      email,
+    });
+    // Se existir retornar User
+    if (userExists) {
+      return userExists;
+    }
+
+    const user = this.usersRepository.create({
+      email,
+    });
+    // se não existir salvar no DB
+    await this.usersRepository.save(user);
+    return user;
+  }
+}
+
+export { UsersService };
