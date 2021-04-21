4c7815a4b0a17d04380d279a4a8fa22c1cf6798e
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-04-21 10:19:48 -0300

    feat: create first entities and repositories

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,7 @@
+import { Repository, EntityRepository } from "typeorm";
+import { Setting } from "../entities/Setting";
+
+@EntityRepository(Setting)
+class SettingsRepository extends Repository<Setting> {}
+
+export { SettingsRepository };
